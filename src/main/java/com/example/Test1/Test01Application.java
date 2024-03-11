33f03eb0ed440c183a30a83f924f9e792315fe24
Mishan Tharindu <v.mishantharindu@gmail.com>
2024-03-11 15:04:04 +0530

    Project Create

---
+++ src/main/java/com/example/Test1/Test01Application.java
@@ -0,0 +1,15 @@
+package com.example.Test1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test01Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Test01Application.class, args);
+
+	}
+
+}
